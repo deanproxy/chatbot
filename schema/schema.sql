c8316e91c9fd5f72b262020ac9bee05496d5a7e9
dean <dean@deanproxy.com>
2016-02-16 15:57:05 -0500

    fix schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -17,6 +17,11 @@ create table jokes(
     text blob
 );
 
+create table quotes(
+    id integer primary key autoincrement, 
+    text blob
+);
+
 insert into jokes(text) values('Why was the robot angry? ...because someone kept pushing his buttons!');
 insert into jokes(text) values("What's a robots favorite type of music?... Heavy Metal!");
 insert into jokes(text) values("What do you get when you cross a robot and a tractor?... A Trans-Farmer! Get it? GET IT?!");
@@ -37,3 +42,12 @@ insert into jokes(text) values("Two guys walk into a bar, the third one ducks.")
 insert into jokes(text) values("I had a dream I was a muffler last night. I woke up exhausted!");
 insert into jokes(text) values("What is Beethoven's favorite fruit? A Ba-na-na-na.");
 insert into jokes(text) values("Did you know that 5/4 people admit they're bad with fractions?");
+
+insert into quotes(text) values("What is this? A center for ants? How can we be expected to teach children to learn how to read... if they can't even fit inside the building?");
+insert into quotes(text) values("There was a moment last night, when she was sandwiched between the two Finnish dwarves and the Maori tribesmen, where I thought, 'Wow, I could really spend the rest of my life with this woman.'");
+insert into quotes(text) values("But why male models?");
+insert into quotes(text) values("They're in the computer... It's so simple!");
+insert into quotes(text) values("I'm not an ambi-turner.");
+insert into quotes(text) values("It's a walk-off!");
+insert into quotes(text) values("Han-sell-out is about to have his Han-sell-ass handed to him on a platter. With french-fried potatoes!");
+insert into quotes(text) values("I think I'm getting the black lung, Pa..");
